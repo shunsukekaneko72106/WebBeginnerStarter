c6221bbba90596552e4b843529c05fac9be14b53
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2023-08-11 14:13:41 +0900

    サーバー起動確認

--- src/main/java/com/example/demo/app/SampleController.java
+++ src/main/java/com/example/demo/app/SampleController.java
@@ -1,24 +1,17 @@
 package com.example.demo.app;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-/*
- * Add annotations here
- */
+@Controller
+@RequestMapping("/sample")
 public class SampleController {
 	
-// 	private final JdbcTemplate jdbcTemplate;
-
-// 	//Add an annotation here 
-// 	public SampleController(JdbcTemplate jdbcTemplate) {
-// 		this.jdbcTemplate = jdbcTemplate;
-// 	}
-	
-	@GetMapping
+	@GetMapping("/test")
 	public String test(Model model) {
-		
-		//hands-on
+		model.addAttribute("title", "Inquiy Form");
 
 		return "test";
 	}
